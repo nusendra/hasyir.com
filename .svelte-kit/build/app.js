ea8ae8acb2cf34a21ca1d88984d9da4dbe8ac4ed
Nusendra Hanggarawan <admin@nusendra.com>
2021-08-21 17:17:26 +0700

    chore: add static adapter

---
+++ .svelte-kit/build/app.js
@@ -0,0 +1,104 @@
+import { respond } from '@sveltejs/kit/ssr';
+import root from './generated/root.svelte';
+import { set_paths, assets } from './runtime/paths.js';
+import { set_prerendering } from './runtime/env.js';
+import * as user_hooks from "./hooks.js";
+
+const template = ({ head, body }) => "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<link rel=\"icon\" href=\"/favicon.png\" />\n\t\t<link\n\t\t\trel=\"stylesheet\"\n\t\t\thref=\"https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css\"\n\t\t\tintegrity=\"sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK\"\n\t\t\tcrossorigin=\"anonymous\"\n\t\t/>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\t\t" + head + "\n\t</head>\n\t<body>\n\t\t<div id=\"svelte\">" + body + "</div>\n\n\t\t<script\n\t\t\tsrc=\"https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js\"\n\t\t\tintegrity=\"sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD\"\n\t\t\tcrossorigin=\"anonymous\"\n\t\t></script>\n\t</body>\n</html>\n";
+
+let options = null;
+
+const default_settings = { paths: {"base":"","assets":""} };
+
+// allow paths to be overridden in svelte-kit preview
+// and in prerendering
+export function init(settings = default_settings) {
+	set_paths(settings.paths);
+	set_prerendering(settings.prerendering || false);
+
+	const hooks = get_hooks(user_hooks);
+
+	options = {
+		amp: false,
+		dev: false,
+		entry: {
+			file: assets + "/_app/start-a624e492.js",
+			css: [assets + "/_app/assets/start-61d1577b.css"],
+			js: [assets + "/_app/start-a624e492.js",assets + "/_app/chunks/vendor-bad684d3.js"]
+		},
+		fetched: undefined,
+		floc: false,
+		get_component_path: id => assets + "/_app/" + entry_lookup[id],
+		get_stack: error => String(error), // for security
+		handle_error: (error, request) => {
+			hooks.handleError({ error, request });
+			error.stack = options.get_stack(error);
+		},
+		hooks,
+		hydrate: true,
+		initiator: undefined,
+		load_component,
+		manifest,
+		paths: settings.paths,
+		prerender: true,
+		read: settings.read,
+		root,
+		service_worker: null,
+		router: true,
+		ssr: true,
+		target: "#svelte",
+		template,
+		trailing_slash: "never"
+	};
+}
+
+const d = decodeURIComponent;
+const empty = () => ({});
+
+const manifest = {
+	assets: [{"file":"favicon.png","size":1571,"type":"image/png"}],
+	layout: ".svelte-kit/build/components/layout.svelte",
+	error: ".svelte-kit/build/components/error.svelte",
+	routes: [
+		{
+						type: 'page',
+						pattern: /^\/$/,
+						params: empty,
+						a: [".svelte-kit/build/components/layout.svelte", "src/routes/index.svelte"],
+						b: [".svelte-kit/build/components/error.svelte"]
+					}
+	]
+};
+
+// this looks redundant, but the indirection allows us to access
+// named imports without triggering Rollup's missing import detection
+const get_hooks = hooks => ({
+	getSession: hooks.getSession || (() => ({})),
+	handle: hooks.handle || (({ request, resolve }) => resolve(request)),
+	handleError: hooks.handleError || (({ error }) => console.error(error.stack)),
+	externalFetch: hooks.externalFetch || fetch
+});
+
+const module_lookup = {
+	".svelte-kit/build/components/layout.svelte": () => import("./components/layout.svelte"),".svelte-kit/build/components/error.svelte": () => import("./components/error.svelte"),"src/routes/index.svelte": () => import("../../src/routes/index.svelte")
+};
+
+const metadata_lookup = {".svelte-kit/build/components/layout.svelte":{"entry":"layout.svelte-e6b70c0a.js","css":[],"js":["layout.svelte-e6b70c0a.js","chunks/vendor-bad684d3.js"],"styles":[]},".svelte-kit/build/components/error.svelte":{"entry":"error.svelte-20aa394e.js","css":[],"js":["error.svelte-20aa394e.js","chunks/vendor-bad684d3.js"],"styles":[]},"src/routes/index.svelte":{"entry":"pages/index.svelte-b4c72829.js","css":[],"js":["pages/index.svelte-b4c72829.js","chunks/vendor-bad684d3.js"],"styles":[]}};
+
+async function load_component(file) {
+	const { entry, css, js, styles } = metadata_lookup[file];
+	return {
+		module: await module_lookup[file](),
+		entry: assets + "/_app/" + entry,
+		css: css.map(dep => assets + "/_app/" + dep),
+		js: js.map(dep => assets + "/_app/" + dep),
+		styles
+	};
+}
+
+export function render(request, {
+	prerender
+} = {}) {
+	const host = request.headers["host"];
+	return respond({ ...request, host }, options, { prerender });
+}
